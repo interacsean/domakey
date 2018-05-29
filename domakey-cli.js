07cdf17031db2d66cc4f8ae8615bdc6cf1a3f340
Sean H <seanhasselback@gmail.com>
2018-05-30 07:38:48 +1000

    Refactor with psuedo-eithers to enable better error handling and smarter code completion

--- domakey-cli.js
+++ domakey-cli.js
@@ -18,19 +18,39 @@ if (cliArgsLine < 1) {
 }
 
 const entityName = cliArgsLine[0];
-try {
-  const entity = require(`${process.cwd()}/.domakey/${entityName}.dmktpl`); 
-  entity({
-    name: entityName,
-    list: cliArgsLine.slice(1),
-    flags: cliArgsFlags,
-    makey: domakeycore,
-  });
+const fileNames = [`${entityName}.js`, entityName];
+let entity = fileNames.reduce((acc, fileName) => {
+  if (acc.isRight) return acc;
+  try {
+    const incEntity = require(`${process.cwd()}/.domakey/${entityName}`);
+    return { isRight: true, val: incEntity };
+  } catch (e) {
+    return { isRight: false, val: e };
+  }
+}, {isRight: false, val: null});
 
-} catch (e) {
-  if (e instanceof Error && e.code === "MODULE_NOT_FOUND")
-    process.stdout.write(`domakey says: I couldn't find the template ${entityName} in \`${process.cwd()}/.domakey/\`\n`)
-  else
-    throw e;
+const completed = ((entity) => {
+  if (entity.isRight) {
+    try {
+      entity.val({
+        name: entityName,
+        list: cliArgsLine.slice(1),
+        flags: cliArgsFlags,
+        makey: domakeycore,
+      });
+      return { isRight: true, val: 'completed' };
+    } catch (e) {
+      return { isRight: false, val: e };
+    }
+  }
+  return entity;
+}).call(null, entity);
+
+if (!completed.isRight) {
+  process.stdout.write(
+    (completed.val instanceof Error && completed.val.code === "MODULE_NOT_FOUND")
+      ? `domakey says "I couldn't find any matching template name ${entityName} in \`${process.cwd()}/.domakey/\`"\n\n`
+      : `domakey says: "Unexpected error, ${completed.val.message}"\n\n`
+  );
   process.exit();
 }
